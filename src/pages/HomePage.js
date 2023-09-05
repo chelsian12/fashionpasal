2282affda53c530b7bc076b287917242ee8235e2
chelsian12 <139612214+chelsian12@users.noreply.github.com>
2023-09-05 16:55:33 +0545

    a

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,15 @@
 
 import HeroSection from "../section-home/HeroSection"
 import Newsletter from "../section-home/Newsletter"
+import Shop from "./Shop"
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <Shop />
       <Newsletter />
+
     </div>
   )
 }
